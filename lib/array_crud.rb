982321955efa0d70b0769923bdc9e420a172c6eb
Thomas Pope <tpope0928@gmail.com>
2018-08-07 20:48:56 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   [1, 2, 3, 4]
   add_element_to_end_of_array << 5
-  puts 
+  puts add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
